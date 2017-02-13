c4dbd0eca9527b036c3c180d492c3ab815d06955
chris-e-watson <chris@watson.me.uk>
2017-02-13 18:51:21 +0000

    Added Manufacturer and Product model types.

---
+++ src/Supermarket/Supermarket.Model/Manufacturer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.Model
+{
+    public class Manufacturer
+    {
+        #region Public Properties
+
+        public int Id { get; set; }
+
+
+        public string Name { get; set; }
+
+
+        public virtual ICollection<Product> Products { get; set; }
+
+        #endregion
+
+        #region Public Constructors
+
+        public Manufacturer()
+        { }
+
+        #endregion
+    }
+}
---
+++ src/Supermarket/Supermarket.Model/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.Model
+{
+    public class Product
+    {
+        #region Public Properties
+
+        public int Id { get; set; }
+
+
+        public virtual Manufacturer Manufacturer { get; set; }
+
+
+        public int ManufacturerId { get; set; }
+
+
+        public string Name { get; set; }
+
+        #endregion
+
+        #region Public Constructors
+
+        public Product()
+        { }
+
+        #endregion
+    }
+}
