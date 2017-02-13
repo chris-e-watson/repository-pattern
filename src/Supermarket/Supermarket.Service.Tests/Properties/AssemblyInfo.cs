a9cd1bba6c06ab14ba025f208bbaf14a67873562
chris-e-watson <chris@watson.me.uk>
2017-02-13 18:41:24 +0000

    Added Supermarket.Service.Tests project.

---
+++ src/Supermarket/Supermarket.Service.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Note: Shared assembly attributes can be found in SharedAssemblyInfo.cs.
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Supermarket.Service.Tests")]
+[assembly: AssemblyDescription("Supermarket.Service.Tests")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("63d980bc-cd22-4b7f-8602-e8049297b62d")]
