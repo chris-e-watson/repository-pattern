4f2782e6faf91e5c72677f77931407baf43edc0e
chris-e-watson <chris@watson.me.uk>
2017-02-13 18:24:14 +0000

    Added SharedAssemblyInfo.cs file.

--- src/Supermarket/Supermarket.Model/Properties/AssemblyInfo.cs
+++ src/Supermarket/Supermarket.Model/Properties/AssemblyInfo.cs
@@ -2,16 +2,13 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// Note: Shared assembly attributes can be found in SharedAssemblyInfo.cs.
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Supermarket.Model")]
 [assembly: AssemblyDescription("Supermarket.Model")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Chris Watson")]
-[assembly: AssemblyProduct("Supermarket")]
-[assembly: AssemblyCopyright("Copyright © Chris Watson 2017")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -21,16 +18,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("45be4316-0ea4-4557-afc2-bfbe09955ebb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
